48652100eaf0f0dc467154c13758c49d5edbf76c
Marcin Sliwinski <msliwinski90@gmail.com>
2012-10-30 11:38:22 +0100

    moja fortunka

--- fortunka/config/initializers/secret_token.rb
+++ fortunka/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fortunka::Application.config.secret_token = '036b2a1c91c32a774b49f2881efa9ba8d9ed5688a6cbfb6f955c1b2674dee71ceb8fe658e4fb586eab6cb8182edf6b0e0d57c3d6544810305a6866b447f50c88'
+Fortunka::Application.config.secret_token = 'a2d5a0089872b8a41db5f5d581ff2e145230ae763ff825a0b4f24199689c215eeb5b62c476f74faa1f91efe11878c33d81ffdc363f0207a8bcc132ad9f70a53f'
